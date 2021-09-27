fdf26783689815860a8cdf56a75391f97a1cbcad
Todd Resudek <toddresudek@gmail.com>
2021-09-27 14:06:55 -0600

    also writes erlang data file

--- lib/spdx.ex
+++ lib/spdx.ex
@@ -7,6 +7,8 @@ defmodule SPDX do
     |> Jason.decode!()
     |> simplify_list()
     |> write_to_file()
+
+    :ok
   end
 
   defp simplify_list(full_data) do
@@ -20,8 +22,19 @@ defmodule SPDX do
   end
 
   defp write_to_file(data) do
+    contents = inspect(data, limit: :infinity)
     [File.cwd!(), "spdx_list.exs"]
     |> Path.join()
-    |> File.write!(inspect(data, limit: :infinity))
+    |> File.write!(contents)
+
+    write_erlterm_file()
+  end
+
+  defp write_erlterm_file() do
+    f = Code.eval_file("spdx_list.exs")
+    f = elem(f, 0)
+    {:ok, file} = :file.open("spdx_list.erlterm", [:write])
+    :io.fwrite(file, "~p~n", [f])
+    :file.close(file)
   end
 end
